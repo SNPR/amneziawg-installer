68f954b6bad40d260c5dcf7954162b4629078d88
Ivan Bondarev <ivan@bondarev.net>
2026-03-31 19:26:39 +0300

    chore: bump version to 5.7.11

--- install_amneziawg.sh
+++ install_amneziawg.sh
@@ -8,7 +8,7 @@ fi
 # ==============================================================================
 # Скрипт для установки и настройки AmneziaWG 2.0 на Ubuntu/Debian серверах
 # Автор: @bivlked
-# Версия: 5.7.10
+# Версия: 5.7.11
 # Дата: 2026-03-31
 # Репозиторий: https://github.com/bivlked/amneziawg-installer
 # ==============================================================================
@@ -16,7 +16,7 @@ fi
 # --- Безопасный режим и Константы ---
 set -o pipefail
 
-SCRIPT_VERSION="5.7.10"
+SCRIPT_VERSION="5.7.11"
 AWG_DIR="/root/awg"
 CONFIG_FILE="$AWG_DIR/awgsetup_cfg.init"
 STATE_FILE="$AWG_DIR/setup_state"
